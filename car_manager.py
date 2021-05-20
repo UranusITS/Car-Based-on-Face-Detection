f294ff96209ce261b3617d32b8dff01a2ce3e65e
Uranus <whlisikuang@outlook.com>
2021-05-20 19:55:56 +0800

    build client socket

--- car_manager.py
+++ car_manager.py
@@ -1,31 +1,61 @@
 import cv2
+import socket
 from face_detector import FaceDetector
 
 
 class CarManager:
-    def __init__(self, min_dx=20, min_da=20, debug=False):
-        self._face_detector = FaceDetector()
+    def __init__(self, min_dx=20, min_da=20, init_num=10, debug=False,
+                 socket_host='172.18.22.12', socket_port=1919):
+        # Init face detection on camera
+        self._face_detector = FaceDetector(debug=debug)
         self._min_dx = min_dx
         self._min_da = min_da
+        self._init_num = init_num
         self._debug = debug
+        self._x = int()
+        self._a = int()
+        self.init_face()
+        # Init socket connection to car
+        self._socket_host = socket_host
+        self._socket_port = socket_port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self._socket.connect((self._socket_host, self._socket_port))
+            print('Socket connection accepted')
+        except socket.timeout:
+            print('Socket connection timeout')
+            exit(1)
+
+    def init_face(self):
+        print('Print \'s\' to start initialization')
+        while True:
+            self._face_detector.get_face_pos()
+            if cv2.waitKey(5) & 0xFF == ord('s'):
+                break
+        print('Init face position')
+        for i in range(self._init_num):
+            x, y, a, fd = self._face_detector.get_face_pos()
+            while not fd:
+                x, y, a, fd = self._face_detector.get_face_pos()
+            self._x += x
+            self._a += a
+        self._x //= self._init_num
+        self._a //= self._init_num
+        print('Init finished')
 
     def run(self):
-        x, y, a, fd = self._face_detector.get_face_pos()
-        while not fd:
-            x, y, a, fd = self._face_detector.get_face_pos()
         while True:
-            xx, yy, aa, fd = self._face_detector.get_face_pos()
+            x, y, a, fd = self._face_detector.get_face_pos()
             if fd:
-                if aa - a > self._min_da:
+                if a - self._a > self._min_da:
                     self.accelerate()
-                elif a - aa > self._min_da:
+                elif self._a - a > self._min_da:
                     self.decelerate()
-                if xx - x > self._min_dx:
+                if x - self._x > self._min_dx:
                     self.turn_left()
-                elif x - xx > self._min_dx:
+                elif self._x - x > self._min_dx:
                     self.turn_right()
-                x, y, a = xx, yy, aa
-            if cv2.waitKey(5) & 0xFF == ord('q'):
+            if cv2.waitKey(5) & 0xFF == 27:
                 break
         self.stop()
 
@@ -34,17 +64,21 @@ class CarManager:
         cv2.destroyAllWindows()
 
     def accelerate(self):
+        self._socket.send(str.encode('accel', 'ASCII'))
         if self._debug:
             print('Accelerate')
 
     def decelerate(self):
+        self._socket.send(str.encode('decel', 'ASCII'))
         if self._debug:
             print('Decelerate')
 
     def turn_left(self):
+        self._socket.send(str.encode('left', 'ASCII'))
         if self._debug:
             print('Turn Left')
 
     def turn_right(self):
+        self._socket.send(str.encode('right', 'ASCII'))
         if self._debug:
             print('Turn Right')
--- face_detector.py
+++ face_detector.py
@@ -2,15 +2,18 @@ import cv2
 
 
 class FaceDetector:
-    def __init__(self, video=0, cascade_classifier_file='haarcascade_frontalface_default.xml'):
+    def __init__(self, video=0, cascade_classifier_file='haarcascade_frontalface_default.xml',
+                 debug=False):
         self._capture = cv2.VideoCapture(video, cv2.CAP_DSHOW)
         self._classifier = cv2.CascadeClassifier(cascade_classifier_file)
+        self._debug = debug
 
     def get_face_pos(self):
         ret, frame = self._capture.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         face = self._classifier.detectMultiScale(gray, 1.1, 3, cv2.CASCADE_FIND_BIGGEST_OBJECT, (175, 175))
-        print(face)
+        if self._debug:
+            print(face)
         if not isinstance(face, tuple):
             x, y, w, h = face[0]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
